f254a47b75c4bbaafbacc05da8bce1e048d24fcc
Anurag Pramod <52824873+anuragpramod@users.noreply.github.com>
2026-06-06 09:37:31 -0400

    feat: create function to play 1 round

--- script.js
+++ script.js
@@ -14,19 +14,79 @@ function getComputerChoice() {
 
 
 function getHumanChoice() {
-    let choice = "scissors";
-    switch (prompt("Enter rock, paper or scissors.").toLowerCase) {
-        case "rock":
-            choice = "rock";
-            break;
-        case "paper":
-            choice = "paper";
-        default:
-            break;
-    } 
+    let choice = prompt("Enter rock, paper or scissors.");
     return choice;
 }
 
 
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
+let computerScore = 0;
+let humanScore = 0;
+
+
+function playRound(computerChoice,humanChoice) {
+    switch (computerChoice) {
+        case "rock":
+            switch (humanChoice.toLowerCase()) {
+                case "rock":
+                    return "This round is a tie.";
+                    break;
+                case "paper":
+                    humanScore++;
+                    return "Human wins this round.";
+                    break;
+                case "scissors":
+                    computerScore++;
+                    return "Computer wins this round.";
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "paper":
+            switch (humanChoice.toLowerCase()) {
+                case "rock":
+                    computerScore++;
+                    return "Computer wins this round.";
+                    break;
+                case "paper":
+                    return "This round is a tie.";
+                    break;
+                case "scissors":
+                    humanScore++;
+                    return "Human wins this round.";
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (humanChoice.toLowerCase()) {
+                case "rock":
+                    humanScore++;                    
+                    return "Human wins this round.";
+                    break;
+                case "paper":
+                    computerScore++;
+                    return "Computer wins this round.";
+                    break;
+                case "scissors":
+                    return "This round is a tie.";
+                    break;
+                default:
+                    break;    
+            }
+            break;
+        default:
+            break;
+    }    
+}
+
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+const result = playRound(computerSelection,humanSelection)
+
+console.log("Computer - " + computerSelection);
+console.log("Human - " + humanSelection);
+console.log(result);
