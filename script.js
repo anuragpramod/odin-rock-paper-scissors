f99b4ba477dcb29c3fd712b2d1b6c9d8c45dcff3
Anurag Pramod <52824873+anuragpramod@users.noreply.github.com>
2026-06-06 08:27:33 -0400

    feat: create function to get human choice

--- script.js
+++ script.js
@@ -14,4 +14,22 @@ function getComputerChoice() {
 
 const computerChoice = getComputerChoice();
 
-console.log(computerChoice);
+
+function getHumanChoice() {
+    let choice = "scissors";
+    switch (prompt("Enter 1 for rock, 2 for paper and 3 for scissors.")) {
+        case "1":
+            choice = "rock";
+            break;
+        case "2":
+            choice = "paper";
+        default:
+            break;
+    } 
+    return choice;
+}
+
+const humanChoice = getHumanChoice();
+
+console.log("Human - " + humanChoice);
+console.log("Computer - " + computerChoice);
