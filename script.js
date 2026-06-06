c34b7b8e60dcadf00e6db14584f86bb14bfda6a6
Anurag Pramod <52824873+anuragpramod@users.noreply.github.com>
2026-06-06 08:10:32 -0400

    feat: add function to generate computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let choice = "scissors";
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper";
+        default:
+            break;
+    } 
+    return choice;
+}
+
+const computerChoice = getComputerChoice();
+
+console.log(computerChoice);
